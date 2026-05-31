859b5b0207ada0f2b12d54628f0f59f7de7bcdfb
TuongNG59 <nhuynhtuong59@gmail.com>
2026-05-31 22:46:52 +0700

    First Commit

---
+++ QuanLyPhongTro/src/main/java/com/qlpt/nguyenhuynhtuong65134116/QuanLyPhongTroApplication.java
@@ -0,0 +1,13 @@
+package com.qlpt.nguyenhuynhtuong65134116;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyPhongTroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyPhongTroApplication.class, args);
+	}
+
+}
