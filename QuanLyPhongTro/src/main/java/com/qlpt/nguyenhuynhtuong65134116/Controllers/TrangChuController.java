65afd372bff193a296d9fdf90499582e0d738966
TuongNG59 <nhuynhtuong59@gmail.com>
2026-06-01 01:47:40 +0700

    Cập nhật build.gradle, tạo vào code TrangChuController, trangchu.html

---
+++ QuanLyPhongTro/src/main/java/com/qlpt/nguyenhuynhtuong65134116/Controllers/TrangChuController.java
@@ -0,0 +1,27 @@
+package com.qlpt.nguyenhuynhtuong65134116.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.qlpt.nguyenhuynhtuong65134116.Services.PhongTroService;
+
+@Controller
+public class TrangChuController {
+
+    @Autowired
+    private PhongTroService phongTroService;
+
+    @GetMapping("/")
+    public String xemTrangChu(Model model) {
+        // Gọi Service lấy danh sách các phòng có trạng thái "TRONG"
+        var danhSachPhong = phongTroService.getPhongTrong();
+        
+        // Đóng gói danh sách này vào Model để gửi sang file HTML
+        model.addAttribute("danhSachPhong", danhSachPhong);
+        
+        // Trả về tên file HTML là "trangchu" (Spring Boot tự hiểu là trangchu.html)
+        return "trangchu";
+    }
+}
